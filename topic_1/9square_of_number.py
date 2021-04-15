9baae1f30be249466cdad68fdc1e7bf03b57fcdf
VladBaryliuk <vladmain9@gmail.com>
2021-04-15 12:33:37 +0300

    doing tasks

--- topic_1/9square_of_number.py
+++ topic_1/9square_of_number.py
@@ -1,5 +1,9 @@
-number = "179"
-for i in range (51):
-    number += "179"
-number = int(number)
-number ** 2
+def square_of_number():
+    number = "179"
+    number_for_sum = "179"
+    for i in range (49):
+        number = number + number_for_sum
+    number = int(number)
+    number = number ** 2
+    print(number)
+square_of_number()
